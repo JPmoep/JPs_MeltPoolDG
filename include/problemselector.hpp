7e81ba93340147b82b48fdf21d8ba45a836e2262
Magdalena Karoline Schreter <magdalena.schreter@uibk.ac.at>
2020-09-07 14:26:17 +0200

    typedef problemselector removed

--- include/problemselector.hpp
+++ include/problemselector.hpp
@@ -1,8 +1,9 @@
 #pragma once
-// multiphaseflow
-#include "levelset.hpp"
-#include "simulationbase.hpp"
-#include "problembase.hpp"
+// MeltPoolDG
+#include <levelset.hpp>
+#include <reinitialization.hpp>
+#include <simulationbase.hpp>
+#include <problembase.hpp>
 
 //@todo: merge this file with problembase.hpp
 
@@ -13,20 +14,18 @@ namespace MeltPoolDG
   template<int dim, int degree>
   class ProblemSelector
   {
-    private:
-      typedef ProblemBase<dim,degree> ProblemType;
     public:
       static 
       std::shared_ptr<ProblemBase<dim,degree>> 
       get_problem( std::shared_ptr<SimulationBase<dim>> sim )
       {
-        std::shared_ptr<ProblemType> problem;
+        std::shared_ptr<ProblemBase<dim,degree>> problem;
         if( sim->parameters.problem_name == "levelset" )
           return std::make_shared<LevelSetEquation<dim,degree>>(sim);     
         if( sim->parameters.problem_name == "reinitialization" )
           return std::make_shared<Reinitialization<dim,degree>>(sim);     
         else
-          AssertThrow(false, ExcMessage("The solver for your requested problem solver does not exist"));
+          AssertThrow(false, ExcMessage("The solver for your requested problem type does not exist"));
       }  
   };
 } // namespace MeltPoolDG
